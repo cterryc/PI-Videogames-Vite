eb246ddb89cff8238cca0a8e265dd74da70fd56f
Terry <danyel.martel@gmail.com>
2023-07-26 20:04:46 -0500

    agregando posgres db_port

--- api/src/config/db.js
+++ api/src/config/db.js
@@ -3,9 +3,14 @@ import { Sequelize } from 'sequelize'
 
 // carga el archivo ".env" y se asignan los valores definidas en él a las variables de entorno de la aplicacion.
 dotenv.config()
-const { PORT_DB, USER_DB, PASS_DB, NAME_DB } = process.env
+const { PORT_DB, USER_DB, PASS_DB, NAME_DB, DB_DEPLOY } = process.env
 
-const DATA_BASE = new Sequelize(`postgres://${USER_DB}:${PASS_DB}@${PORT_DB}/${NAME_DB}`, {
+// const DATA_BASE = new Sequelize(`postgres://${USER_DB}:${PASS_DB}@${PORT_DB}/${NAME_DB}`, {
+//   logging: false
+// })
+
+// "?sslmode=require" es necesario para activar la seguridad SSL al ser desplegado en la nube
+const DATA_BASE = new Sequelize(`${DB_DEPLOY}?sslmode=require`, {
   logging: false
 })
 
