f54b0ad9a3783a50ce836a6e4515a147ee50d075
Terry <danyel.martel@gmail.com>
2023-08-11 16:14:16 -0500

    set new url

--- client/src/redux/editable-stuff/editableConfig.js
+++ client/src/redux/editable-stuff/editableConfig.js
@@ -1,3 +1,3 @@
-export const API = 'http://localhost:3001'
+export const API = 'https://videogames-vite.onrender.com'
 
 export const imagenDeRespaldo = 'https://c.wallhere.com/photos/e4/13/World_of_Warcraft_Chenbo_elves_fantasy_art_video_games_digital_art_Night_Elves-357408.jpg!d'
